cab8096eb8026554dd97d7e7080e58d8b60e4bac
Fernando Guevara <ferbusson@gmail.com>
2023-12-15 11:40:16 -0500

    se corrigen problemas de visualizacion vista Despacho Vehiculos para demo

--- app/src/main/java/com/example/emakumovil/modules/ventas/DespachoVehiculos.java
+++ app/src/main/java/com/example/emakumovil/modules/ventas/DespachoVehiculos.java
@@ -3,20 +3,12 @@ package com.example.emakumovil.modules.ventas;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageButton;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.TableLayout;
-import android.widget.TextView;
 
 import com.example.emakumovil.R;
 import com.example.emakumovil.components.AnswerEvent;
 import com.example.emakumovil.components.AnswerListener;
 import com.example.emakumovil.components.DialogClickEvent;
 import com.example.emakumovil.components.DialogClickListener;
-import com.example.emakumovil.components.SearchQuery;
 
 public class DespachoVehiculos extends Activity implements View.OnClickListener, DialogClickListener,
         AnswerListener, View.OnFocusChangeListener {
@@ -24,7 +16,7 @@ public class DespachoVehiculos extends Activity implements View.OnClickListener,
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         // hace que al dar clic en menu Tiquete se muestre la interfaz de tiquetes
-        setContentView(R.layout.activity_despacho_vahiculo);
+        setContentView(R.layout.activity_despacho_vehiculo);
         // consulta el punto origen basado en el usuario
         //new SearchQuery(this,"MVSEL0082",args).start();
         // boton lupa buscar destino
